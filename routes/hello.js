a8b33d12cf59d18a2287979f001f1e821c6a926a
Kasey Chen <ksc003@ucsd.edu>
2021-01-28 11:25:26 -0800

    node.ja lab

--- routes/hello.js
+++ routes/hello.js
@@ -1,10 +1,10 @@
-
 /*
  * GET home page.
  */
 
 exports.view = function(req, res){
-  res.render('index', {
-  	'name': '',
+  var nameToShow = req.params.userName;
+  res.render("hello", {
+    "name": nameToShow
   });
 };
